af730f2c1894f0e294d295e9ae8157f48d79cf6d
mariari <mariari@protonmail.com>
2019-11-14 08:57:26 -0500

    made the eac node type along with laying the groundwork for the llvm env

--- src/Juvix/Backends/LLVM/Net/EAC.hs
+++ src/Juvix/Backends/LLVM/Net/EAC.hs
@@ -1,3 +1,20 @@
 module Juvix.Backends.LLVM.Net.EAC where
 
--- Create a tag type
+import qualified Juvix.Backends.LLVM.Codegen as Codegen
+import qualified Juvix.Backends.LLVM.Net.EAC.Types as Types
+import Juvix.Library
+
+--------------------------------------------------------------------------------
+-- Reduction rules
+--------------------------------------------------------------------------------
+
+-- TODO ∷ Maybe add metadata at some point?
+
+-- These functions work off the nodeType signature not Types.eac
+
+-- mimic rules from the interpreter
+anihilateRewireAux = undefined
+  where
+    args = [(Codegen.nodeType, "node_1"), (Codegen.nodeType, "node_2")]
+    body = do
+      undefined
--- src/Juvix/Backends/LLVM/Net/EAC/Types.hs
+++ src/Juvix/Backends/LLVM/Net/EAC/Types.hs
@@ -1,1 +1,20 @@
 module Juvix.Backends.LLVM.Net.EAC.Types where
+
+import qualified Juvix.Backends.LLVM.Codegen as Codegen
+import Juvix.Library
+-- Abstract out LLVM imports?
+import qualified LLVM.AST.Type as Type
+
+-- defines a tag over a node type, to denote the variant
+
+tag ∷ Type.Type
+tag = Codegen.i4
+
+eac ∷ Type.Type
+eac = Type.StructureType
+  { Type.isPacked = True,
+    Type.elementTypes =
+      [ tag,
+        Codegen.nodeType
+      ]
+  }
