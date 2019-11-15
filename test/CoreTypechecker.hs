9c2e0ace7a217a251cbb208fb0ff4d4f99bf50b8
Marty Stumpf <thealmartyblog@gmail.com>
2019-11-15 13:23:55 -0800

    Add kApp1.

--- test/CoreTypechecker.hs
+++ test/CoreTypechecker.hs
@@ -167,6 +167,25 @@ identityAppK =
         )
     )
 
+-- (K: Nat -> Nat -> Nat 1) should type check to Nat -> Nat
+kApp1 :: NatElim
+kApp1 =
+  IR.App
+    ( IR.Ann
+        (SNat 1)
+        kcombinator
+        ( IR.Pi
+            (SNat 1)
+            (IR.PrimTy Nat)
+            (IR.Pi (SNat 1) (IR.PrimTy Nat) (IR.PrimTy Nat))
+        )
+    )
+    (IR.Elim (IR.Prim (Natural 1)))
+
+natToNatTy :: NatAnnotation
+natToNatTy = 
+  (SNat 1, IR.VPi (SNat 1) (IR.VPrimTy Nat) (const (IR.VPrimTy Nat)))
+
 --K: (Nat -> Nat) -> Nat -> (Nat -> Nat) I:Nat -> Nat type checks to Nat -> (Nat -> Nat)
 kAppI ∷ NatElim
 kAppI =
@@ -200,6 +219,7 @@ kAppICompTy =
       (IR.VPrimTy Nat)
       (const (IR.VPi (SNat 0) (IR.VPrimTy Nat) (const (IR.VPrimTy Nat))))
   )
+  
 
 {-
 -- Because S returns functions, it's not general because of the annotations.
@@ -229,7 +249,7 @@ scombinator =
                         (IR.Ann
                             (SNat 1)
                             (IR.Bound 2)
-                            () -- Annotation of x                  
+                            () -- Annotation of x
                         )
                         (IR.Elim (IR.Bound 0))
                     )
@@ -239,7 +259,7 @@ scombinator =
                     (IR.Ann
                         (SNat 1)
                         (IR.Bound 1)
-                        () -- Annotation of y                  
+                        () -- Annotation of y
                     )
                     (IR.Elim (IR.Bound 0))
                 )
@@ -288,6 +308,9 @@ test_identity_app_k = shouldInfer nat identityAppK kCompTy
 
 test_k_app_I ∷ T.TestTree
 test_k_app_I = shouldInfer nat kAppI kAppICompTy
+
+test_k_app_1 :: T.TestTree
+test_k_app_1 = shouldInfer nat kApp1 natToNatTy
 
 --test_siii :: T.TestTree
 --test_siii = shouldInfer all scombinator
