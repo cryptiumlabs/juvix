4f87a488e686773ade96af4679357d9c9bdedbe0
mariari <mariari@protonmail.com>
2019-11-18 15:50:03 -0500

    made the maybe type and created directory and file type

--- doc/Code/generate.lisp
+++ doc/Code/generate.lisp
@@ -1,4 +1,6 @@
 (ql:quickload "cl-org-mode")
+(ql:quickload "trivia")
+(ql:quickload "fset")
 
 (asdf:load-system :uiop)
 
@@ -8,8 +10,31 @@
 (in-package :code-generation)
 
 ;; (uiop:directory-files "../../holder/")
+;; Maybe type -----------------------------------------------
+;; todo better type it
+(defconstant +nothing+ :none)
+
+(defstruct just val)
+
+(deftype maybe ()
+    `(or
+      (eql :none)
+      (satisfies just-p)))
+
+;; Directory and file types ---------------------------------
+(defstruct org-directory
+  ;; some directories have a file that re-export things
+  (file +nothing+ :type maybe)
+  ;; a dir consists of either a file-info-p or a org-directory-p
+  (dir  nil       :type list))
+
+(defstruct file-info
+  (path  ""        :type pathname)
+  (alias +nothing+ :type maybe))
+
 
 (defun generate-org-file (directory)
+  ;; (labels ((rec (directory conflicts))))
   (let ((files    (uiop:directory-files directory))
         (sub-dirs (uiop:subdirectories directory)))
     ))
