0394b66663aa1d17dc70ecd378affd508636766f
mariari <mariari@protonmail.com>
2019-12-02 14:07:31 -0500

    created the linkAll abstraction

--- src/Juvix/Backends/LLVM/DSL.hs
+++ src/Juvix/Backends/LLVM/DSL.hs
@@ -5,7 +5,11 @@
 --    - gives a declarative way to do a bunch of links and relinks
 module Juvix.Backends.LLVM.DSL where
 
+import qualified Juvix.Backends.LLVM.Codegen as Codegen
 import Juvix.Library hiding (reduce)
+import qualified Juvix.Library.HashMap as Map
+import qualified LLVM.AST.Name as Name
+import qualified LLVM.AST.Operand as Operand
 import Prelude (error)
 
 -- | Type for specifying how one wants to link nodes
@@ -32,6 +36,43 @@ defRel =
 
 -- | REL: a type that displays whether we are linking from an old node or just adding a new link
 data REL node port
-  = Link node
-  | ReLink node port
+  = Link node port
+  | LinkConnected node port
   deriving (Show)
+
+linkAll ∷
+  ( HasThrow "err" Codegen.Errors f,
+    HasState "blocks" (Map.HashMap Name.Name Codegen.BlockState) f,
+    HasState "count" Word f,
+    HasState "currentBlock" Name.Name f,
+    HasState "symtab" Codegen.SymbolTable f,
+    HasState "typTab" Codegen.TypeTable f,
+    HasState "varTab" Codegen.VariantToType f
+  ) ⇒
+  Relink Operand.Operand Operand.Operand →
+  f ()
+linkAll (RelAuxiliary node p a1 a2 a3 a4) = do
+  -- Reodoing Codegen.mainPort/auxiliary* may or may not have an extra cost.
+  -- Find out!
+  let flipHelper p f = p >>= linkHelper f node
+  traverse_ (flipHelper Codegen.mainPort) p
+  traverse_ (flipHelper Codegen.auxiliary1) a1
+  traverse_ (flipHelper Codegen.auxiliary2) a2
+  traverse_ (flipHelper Codegen.auxiliary3) a3
+  traverse_ (flipHelper Codegen.auxiliary4) a4
+
+linkHelper ∷
+  ( HasThrow "err" Codegen.Errors m,
+    HasState "blocks" (Map.HashMap Name.Name Codegen.BlockState) m,
+    HasState "count" Word m,
+    HasState "currentBlock" Name.Name m,
+    HasState "symtab" Codegen.SymbolTable m
+  ) ⇒
+  REL Operand.Operand Operand.Operand →
+  Operand.Operand →
+  Operand.Operand →
+  m ()
+linkHelper (Link nl pl) node port =
+  Codegen.link [node, port, nl, pl]
+linkHelper (LinkConnected nl pl) node port =
+  Codegen.linkConnectedPort [nl, pl, node, port]
