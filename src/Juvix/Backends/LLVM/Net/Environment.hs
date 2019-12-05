9913a6d7941041d0040cdd3915fc0012dc75eef4
mariari <mariari@protonmail.com>
2019-12-04 19:50:54 -0500

    added enviornment module

---
+++ src/Juvix/Backends/LLVM/Net/Environment.hs
@@ -0,0 +1,10 @@
+-- |
+-- - Serves as the default environment for executing EAC code
+module Juvix.Backends.LLVM.Net.Environment where
+
+import qualified Juvix.Backends.LLVM.Codegen as Codegen
+import qualified Juvix.Backends.LLVM.Net.EAC as EAC
+import qualified Juvix.Backends.LLVM.Net.EAC.Defs as Defs
+import Juvix.Library
+
+initialModule = undefined
