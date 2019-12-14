1e74c42e5ce81d7734e49167d537121824f61e3d
Christopher Goes <cwgoes@pluranimity.org>
2019-12-14 03:39:16 +0100

    Add TODOs

--- src/Juvix/Backends/LLVM/Net/API.hs
+++ src/Juvix/Backends/LLVM/Net/API.hs
@@ -16,34 +16,43 @@ defineCreateNet ∷ Codegen.Define m ⇒ m Operand.Operand
 defineCreateNet =
   Codegen.defineFunction Type.void "createNet" [] $
     -- TODO
+    -- Clear the current net
+    -- call appendToNet with nodes
     pure ()
 
 defineReadNet ∷ Codegen.Define m ⇒ m Operand.Operand
 defineReadNet =
   Codegen.defineFunction Type.void "readNet" [] $
     -- TODO
+    -- Read the current net, return a list of nodes
     pure ()
 
 defineSaveState ∷ Codegen.Define m ⇒ m Operand.Operand
 defineSaveState =
   Codegen.defineFunction Type.void "saveState" [] $
     -- TODO
+    -- Save the current net state to some opaque pointer
     pure ()
 
 defineLoadState ∷ Codegen.Define m ⇒ m Operand.Operand
 defineLoadState =
   Codegen.defineFunction Type.void "loadState" [] $
     -- TODO
+    -- Load the current net state from some opaque pointer
     pure ()
 
 defineAppendToNet ∷ Codegen.Define m ⇒ m Operand.Operand
 defineAppendToNet =
   Codegen.defineFunction Type.void "appendToNet" [] $
     -- TODO
+    -- Append nodes to the net
+    -- Call `createNode` on each
+    -- Append to the primary pair list as necessary
     pure ()
 
 defineReduceUntilComplete ∷ Codegen.Define m ⇒ m Operand.Operand
 defineReduceUntilComplete =
   Codegen.defineFunction Type.void "reduceUntilComplete" [] $
     -- TODO
+    -- Call reduce until there are no primary pairs left
     pure ()
