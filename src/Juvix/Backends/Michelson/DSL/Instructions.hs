90a4526f7872a013d245b70bc6ad09faced0efde
mariari <mariari@protonmail.com>
2020-02-06 06:05:48 -0500

    added some starting instructions

--- src/Juvix/Backends/Michelson/DSL/Instructions.hs
+++ src/Juvix/Backends/Michelson/DSL/Instructions.hs
@@ -3,6 +3,39 @@
 --   over the untyped instruction bindings
 module Juvix.Backends.Michelson.DSL.Instructions where
 
+import Juvix.Library
 import qualified Michelson.Untyped.Instr as Instr
+import qualified Michelson.Untyped.Type as Type
+import qualified Michelson.Untyped.Value as Value
+
+car ∷ Instr.ExpandedOp
+car = Instr.PrimEx (Instr.CAR "" "")
 
 
+cdr ∷ Instr.ExpandedOp
+cdr = Instr.PrimEx (Instr.CDR "" "")
+
+
+dup ∷ Instr.ExpandedOp
+dup = Instr.PrimEx (Instr.DUP "")
+
+swap ∷ Instr.ExpandedOp
+swap = Instr.PrimEx Instr.SWAP
+
+dig ∷ Word → Instr.ExpandedOp
+dig = Instr.PrimEx . Instr.DIG
+
+dug ∷ Word → Instr.ExpandedOp
+dug = Instr.PrimEx . Instr.DUG
+
+push ∷ Type.Type → Value.Value' Instr.ExpandedOp → Instr.ExpandedOp
+push typ value = Instr.PrimEx (Instr.PUSH "" typ value)
+
+some ∷ Instr.ExpandedOp
+some = Instr.PrimEx (Instr.SOME "" "")
+
+unit ∷ Instr.ExpandedOp
+unit = Instr.PrimEx (Instr.UNIT "" "")
+
+pair ∷ Instr.ExpandedOp
+pair = Instr.PrimEx (Instr.PAIR "" "" "" "")
