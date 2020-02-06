a9bab7713fb2597fdf71445b53fdc7732cce33ee
mariari <mariari@protonmail.com>
2020-02-06 10:49:23 -0500

    added many more instructions, so many more to generate

--- src/Juvix/Backends/Michelson/DSL/Instructions.hs
+++ src/Juvix/Backends/Michelson/DSL/Instructions.hs
@@ -11,10 +11,8 @@ import qualified Michelson.Untyped.Value as Value
 car ∷ Instr.ExpandedOp
 car = Instr.PrimEx (Instr.CAR "" "")
 
-
 cdr ∷ Instr.ExpandedOp
 cdr = Instr.PrimEx (Instr.CDR "" "")
-
 
 dup ∷ Instr.ExpandedOp
 dup = Instr.PrimEx (Instr.DUP "")
@@ -39,3 +37,129 @@ unit = Instr.PrimEx (Instr.UNIT "" "")
 
 pair ∷ Instr.ExpandedOp
 pair = Instr.PrimEx (Instr.PAIR "" "" "" "")
+
+left ∷ Type.Type → Instr.ExpandedOp
+left = Instr.PrimEx . Instr.LEFT "" "" "" ""
+
+right ∷ Type.Type → Instr.ExpandedOp
+right = Instr.PrimEx . Instr.RIGHT "" "" "" ""
+
+nil ∷ Type.Type → Instr.ExpandedOp
+nil = Instr.PrimEx . Instr.NIL "" ""
+
+cons ∷ Instr.ExpandedOp
+cons = Instr.PrimEx (Instr.CONS "")
+
+size ∷ Instr.ExpandedOp
+size = Instr.PrimEx (Instr.SIZE "")
+
+emptySet ∷ Type.Comparable → Instr.ExpandedOp
+emptySet = Instr.PrimEx . Instr.EMPTY_SET "" ""
+
+emptyMap ∷ Type.Comparable → Type.Type → Instr.ExpandedOp
+emptyMap = (Instr.PrimEx .) . Instr.EMPTY_MAP "" ""
+
+emptyBigMap ∷ Type.Comparable → Type.Type → Instr.ExpandedOp
+emptyBigMap = (Instr.PrimEx .) . Instr.EMPTY_BIG_MAP "" ""
+
+mem ∷ Instr.ExpandedOp
+mem = Instr.PrimEx (Instr.MEM "")
+
+get ∷ Instr.ExpandedOp
+get = Instr.PrimEx (Instr.GET "")
+
+update ∷ Instr.ExpandedOp
+update = Instr.PrimEx (Instr.UPDATE "")
+
+exec ∷ Instr.ExpandedOp
+exec = Instr.PrimEx (Instr.EXEC "")
+
+apply ∷ Instr.ExpandedOp
+apply = Instr.PrimEx (Instr.APPLY "")
+
+cast ∷ Type.Type → Instr.ExpandedOp
+cast = Instr.PrimEx . Instr.CAST ""
+
+rename ∷ Instr.ExpandedOp
+rename = Instr.PrimEx (Instr.RENAME "")
+
+pack ∷ Instr.ExpandedOp
+pack = Instr.PrimEx (Instr.PACK "")
+
+unpack ∷ Type.Type → Instr.ExpandedOp
+unpack = Instr.PrimEx . Instr.UNPACK "" ""
+
+concat ∷ Instr.ExpandedOp
+concat = Instr.PrimEx (Instr.CONCAT "")
+
+slice ∷ Instr.ExpandedOp
+slice = Instr.PrimEx (Instr.SLICE "")
+
+isNat ∷ Instr.ExpandedOp
+isNat = Instr.PrimEx (Instr.ISNAT "")
+
+add ∷ Instr.ExpandedOp
+add = Instr.PrimEx (Instr.ADD "")
+
+sub ∷ Instr.ExpandedOp
+sub = Instr.PrimEx (Instr.SUB "")
+
+mul ∷ Instr.ExpandedOp
+mul = Instr.PrimEx (Instr.MUL "")
+
+ediv ∷ Instr.ExpandedOp
+ediv = Instr.PrimEx (Instr.EDIV "")
+
+abs ∷ Instr.ExpandedOp
+abs = Instr.PrimEx (Instr.ABS "")
+
+neg ∷ Instr.ExpandedOp
+neg = Instr.PrimEx (Instr.NEG "")
+
+lsl ∷ Instr.ExpandedOp
+lsl = Instr.PrimEx (Instr.LSL "")
+
+lsr ∷ Instr.ExpandedOp
+lsr = Instr.PrimEx (Instr.LSR "")
+
+or ∷ Instr.ExpandedOp
+or = Instr.PrimEx (Instr.OR "")
+
+and ∷ Instr.ExpandedOp
+and = Instr.PrimEx (Instr.AND "")
+
+xor ∷ Instr.ExpandedOp
+xor = Instr.PrimEx (Instr.XOR "")
+
+not ∷ Instr.ExpandedOp
+not = Instr.PrimEx (Instr.NOT "")
+
+compare ∷ Instr.ExpandedOp
+compare = Instr.PrimEx (Instr.COMPARE "")
+
+eq ∷ Instr.ExpandedOp
+eq = Instr.PrimEx (Instr.EQ "")
+
+neq ∷ Instr.ExpandedOp
+neq = Instr.PrimEx (Instr.NEQ "")
+
+lt ∷ Instr.ExpandedOp
+lt = Instr.PrimEx (Instr.LT "")
+
+le ∷ Instr.ExpandedOp
+le = Instr.PrimEx (Instr.LE "")
+
+ge ∷ Instr.ExpandedOp
+ge = Instr.PrimEx (Instr.GE "")
+
+int ∷ Instr.ExpandedOp
+int = Instr.PrimEx (Instr.INT "")
+
+self ∷ Instr.ExpandedOp
+self = Instr.PrimEx (Instr.SELF "")
+
+contract ∷ Type.Type → Instr.ExpandedOp
+contract = Instr.PrimEx . Instr.CONTRACT "" ""
+
+transferTokens ∷ Instr.ExpandedOp
+transferTokens = Instr.PrimEx (Instr.TRANSFER_TOKENS "")
--- src/Juvix/Backends/Michelson/DSL/InstructionsEff.hs
+++ src/Juvix/Backends/Michelson/DSL/InstructionsEff.hs
@@ -5,6 +5,9 @@
 --   + So for example, emitting an =add=, eats two items from the
 --     virtual stack, and adds an =Instr.Add= instruction to the
 --     sequence of instructions to execute
+-- - For constant progoation, have a function say take-2 that looks at
+--   the top two items in the stack and then returns back either if
+--   they were constants or not and dispatches logic based on that
 
 module Juvix.Backends.Michelson.DSL.InstructionsEff where
 
