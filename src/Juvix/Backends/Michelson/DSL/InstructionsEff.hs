bd4f2cd75c6c4125f5e0808b1bc228365f653226
mariari <mariari@protonmail.com>
2020-02-12 14:26:50 -0500

    abstracted the + function so it can work pretty much on any operator

--- src/Juvix/Backends/Michelson/DSL/InstructionsEff.hs
+++ src/Juvix/Backends/Michelson/DSL/InstructionsEff.hs
@@ -18,9 +18,21 @@ import qualified Juvix.Library.HashMap as Map
 import qualified Michelson.Untyped.Instr as Instr
 import qualified Michelson.Untyped.Value as V
 
+--------------------------------------------------------------------------------
+-- Top Level Types
+--------------------------------------------------------------------------------
+
+data Top
+  = Curr
+  | Completed Expanded
+
 data Expanded
   = Constant (V.Value' Types.Op)
   | Expanded (Instr.ExpandedOp)
+
+--------------------------------------------------------------------------------
+-- Main Functionality
+--------------------------------------------------------------------------------
 
 envMapping =
   Map.fromList [(Instr.ADD, (+))]
@@ -36,18 +48,47 @@ allConstants = all f
 
 addAll instr1 instr2 = add [instr2, instr1]
 
-add instrs = do
+add = intGen Instructions.add (+)
+
+mul = intGen Instructions.mul (*)
+
+sub = intGen Instructions.sub (-)
+
+ediv =
+  onIntGen
+    Instructions.ediv
+    ( \x y → case y of
+        0 → V.ValueNone
+        y → V.ValueSome (V.ValuePair (V.ValueInt (x `div` y)) (V.ValueInt (rem x y)))
+    )
+
+--------------------------------------------------------------------------------
+-- Helpers
+--------------------------------------------------------------------------------
+
+intGen op f = onIntGen op (\x y → V.ValueInt (f x y))
+
+onIntGen op f =
+  onTwoArgs
+    op
+    ( \instr1 instr2 →
+        let V.ValueInt i1 = instr1
+            V.ValueInt i2 = instr2
+         in Constant (f i1 i2)
+    )
+
+onTwoArgs op f instrs = do
   instr2 : instr1 : _ ← traverse (protect . inst) instrs
   let instrs = [instr2, instr1]
   if
     | allConstants (val <$> instrs) →
-      let Constant (V.ValueInt i1) = val instr1
-          Constant (V.ValueInt i2) = val instr2
-       in pure (Constant (V.ValueInt (i1 + i2)))
+      let Constant i1 = val instr1
+          Constant i2 = val instr2
+       in pure (f i1 i2)
     | otherwise → do
       traverse_ addExpanded instrs
-      addInstr Instructions.add
-      pure (Expanded Instructions.add)
+      addInstr op
+      pure (Expanded op)
 
 data Protect
   = Protect
@@ -63,7 +104,7 @@ protect inst = do
   put @"ops" curr
   pure Protect {val = v, insts = after}
 
--- for the exapnded case it was already added!
+-- for constant intructions the list should be empty!
 addExpanded ∷ Env.Ops m ⇒ Protect → m ()
 addExpanded (Protect (Expanded _) i) = addInstrs i
 addExpanded (Protect (Constant v) _) = addInstr (Instructions.push undefined v)
