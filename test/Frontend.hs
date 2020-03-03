0d8a90bf2fc2cf924927ba0552fb5cf3c3012544
mariari <mariari@protonmail.com>
2020-03-03 10:56:28 -0500

    added some tests

---
+++ test/Frontend.hs
@@ -0,0 +1,27 @@
+module Frontend where
+
+import Data.Attoparsec.ByteString
+import qualified Juvix.Frontend.Parser as Parser
+import Juvix.Library
+
+--------------------------------------------------------------------------------
+-- Spacer tests
+--------------------------------------------------------------------------------
+
+spacerSymb ∷ Bool
+spacerSymb =
+  case parse (Parser.spacer Parser.prefixSymbol) "Foo   f" of
+    Done f s → f == "f" && s == "Foo"
+    _ → False
+
+--------------------------------------------------------------------------------
+-- validPrefixSymbols
+--------------------------------------------------------------------------------
+
+vpsDashFrontFail ∷ Bool
+vpsDashFrontFail =
+  isLeft (parseOnly Parser.prefixSymbol "-Foo")
+
+vpsDashMiddle ∷ Bool
+vpsDashMiddle =
+  isRight (parseOnly Parser.prefixSymbol "Foo-Foo")
