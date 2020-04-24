3367ccfac60ea898159856b6b1e35ff71df1b8f7
Andy Morris <andy@cryptium.ch>
2020-04-25 01:03:49 +0200

    Add globals to typechecker state

--- src/Juvix/Core/IR/Typechecker/Env.hs
+++ src/Juvix/Core/IR/Typechecker/Env.hs
@@ -1,12 +1,56 @@
 module Juvix.Core.IR.Typechecker.Env where
 
-import Juvix.Core.IR.Typechecker.Log
+import Juvix.Library hiding (Datatype)
+import qualified Juvix.Core.IR.Types as IR
+import qualified Juvix.Core.Usage as Usage
 import Juvix.Core.IR.Typechecker.Types
-import Juvix.Library
+import Juvix.Core.IR.Typechecker.Log
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HashMap
+
 
 data EnvCtx primTy primVal
-  = EnvCtx {typecheckerLog :: [Log primTy primVal]}
+  = EnvCtx
+    { typecheckerLog :: [Log primTy primVal]
+    , globals :: Globals primTy primVal
+    }
   deriving (Show, Eq, Generic)
+
+type Globals primTy primVal =
+  HashMap IR.GlobalName (Global primTy primVal)
+
+data Global primTy primVal
+  = GDatatype (Datatype primTy primVal)
+  | GDataCon  (DataCon  primTy primVal)
+  deriving (Show, Eq, Generic)
+
+data Datatype primTy primVal
+  = Datatype
+    { dataName  :: IR.GlobalName
+      -- | the type constructor's arguments
+    , dataArgs  :: [DataArg primTy primVal]
+      -- | the type constructor's target universe level
+    , dataLevel :: Natural
+    , dataCons  :: [DataCon primTy primVal]
+    }
+  deriving (Show, Eq, Generic)
+
+data DataArg primTy primVal
+  = DataArg
+    { argName    :: IR.GlobalName
+    , argUsage   :: Usage.T
+    , argType    :: IR.Value primTy primVal
+    , argIsParam :: Bool
+    }
+  deriving (Show, Eq, Generic)
+
+data DataCon primTy primVal
+  = DataCon
+    { conName :: IR.GlobalName
+    , conType :: IR.Value primTy primVal
+    }
+  deriving (Show, Eq, Generic)
+
 
 type EnvAlias primTy primVal =
   ExceptT (TypecheckError primTy primVal)
@@ -18,12 +62,27 @@ newtype EnvTypecheck primTy primVal a = EnvTyp (EnvAlias primTy primVal a)
     (HasThrow "typecheckError" (TypecheckError primTy primVal))
     via MonadError (EnvAlias primTy primVal)
   deriving
-    ( HasSink "typecheckerLog" [Log primTy primVal],
-      HasWriter "typecheckerLog" [Log primTy primVal]
-    )
-    via WriterField "typecheckerLog" (EnvAlias primTy primVal)
+    (HasSink "typecheckerLog" [Log primTy primVal],
+     HasWriter "typecheckerLog" [Log primTy primVal])
+  via WriterField "typecheckerLog" (EnvAlias primTy primVal)
+  deriving
+    (HasSink   "globals" (Globals primTy primVal),
+     HasState  "globals" (Globals primTy primVal))
+  via StateField "globals" (EnvAlias primTy primVal)
+  deriving
+    (HasSource "globals" (Globals primTy primVal),
+     HasReader "globals" (Globals primTy primVal))
+  via ReaderField "globals" (EnvAlias primTy primVal)
+
 
 exec ::
   EnvTypecheck primTy primVal a ->
   (Either (TypecheckError primTy primVal) a, EnvCtx primTy primVal)
-exec (EnvTyp env) = runState (runExceptT env) (EnvCtx [])
+exec (EnvTyp env) =
+  runState (runExceptT env) $ EnvCtx [] mempty
+
+-- exec :: Globals primTy primVal
+--      -> EnvTypecheck primTy primVal a
+--      -> (Either (TypecheckError primTy primVal) a, EnvCtx primTy primVal)
+-- exec globals (EnvTypecheck env) =
+--   runState (runExceptT env) $ EnvCtx [] globals
