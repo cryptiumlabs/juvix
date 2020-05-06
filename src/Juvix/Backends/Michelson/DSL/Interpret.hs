dc8ec79594c80dee132bb7bee29e8d4a94a814ca
mariari <mariari@protonmail.com>
2020-05-06 13:45:32 -0400

    added some imports

--- src/Juvix/Backends/Michelson/DSL/Interpret.hs
+++ src/Juvix/Backends/Michelson/DSL/Interpret.hs
@@ -1,1 +1,5 @@
 module Juvix.Backends.Michelson.Interpret where
+
+import qualified Juvix.Backends.Michelson.Contract as Contract
+import Juvix.Library
+import qualified Michelson.Interpret as Interpret
