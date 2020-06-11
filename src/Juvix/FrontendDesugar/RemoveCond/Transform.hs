71922757e0be2d68432316bf9d1d46e46f09591d
mariari <mariari@protonmail.com>
2020-06-11 09:47:45 -0400

    finished the expressino and top level boilerplate

--- src/Juvix/FrontendDesugar/RemoveCond/Transform.hs
+++ src/Juvix/FrontendDesugar/RemoveCond/Transform.hs
@@ -28,15 +28,56 @@ transformCond (Old.C xs) =
 -- Boilerplate Transforms
 --------------------------------------------------------------------------------
 transformtTopLevel :: Old.TopLevel -> New.TopLevel
-transformtTopLevel (Old.Type t) = New.Type (transformType t)
-transformtTopLevel (Old.ModuleOpen t) = New.ModuleOpen (transformModuleOpen t)
-transformtTopLevel (Old.Signature t) = New.Signature undefined
-transformtTopLevel (Old.Function t) = New.Function undefined
-transformtTopLevel Old.TypeClass = New.TypeClass
-transformtTopLevel Old.TypeClassInstance = New.TypeClassInstance
+transformtTopLevel (Old.Type t) =
+  New.Type (transformType t)
+transformtTopLevel (Old.ModuleOpen t) =
+  New.ModuleOpen (transformModuleOpen t)
+transformtTopLevel (Old.Signature t) =
+  New.Signature (transformSignature t)
+transformtTopLevel (Old.Function t) =
+  New.Function (transformFunction t)
+transformtTopLevel Old.TypeClass =
+  New.TypeClass
+transformtTopLevel Old.TypeClassInstance =
+  New.TypeClassInstance
 
 transformExpression :: Old.Expression -> New.Expression
-transformExpression = undefined
+transformExpression (Old.Cond c) =
+  transformCond c
+transformExpression (Old.Constant c) =
+  New.Constant (transformConst c)
+transformExpression (Old.Let l) =
+  New.Let (transformLet l)
+transformExpression (Old.LetType l) =
+  New.LetType (transformLetType l)
+transformExpression (Old.Match m) =
+  New.Match (transformMatch m)
+transformExpression (Old.Name n) =
+  New.Name n
+transformExpression (Old.OpenExpr n) =
+  New.OpenExpr (transformModuleOpenExpr n)
+transformExpression (Old.Lambda l) =
+  New.Lambda (transformLambda l)
+transformExpression (Old.Application a) =
+  New.Application (transformApplication a)
+transformExpression (Old.Block b) =
+  New.Block (transformBlock b)
+transformExpression (Old.Infix i) =
+  New.Infix (transformInfix i)
+transformExpression (Old.ExpRecord i) =
+  New.ExpRecord (transformExpRecord i)
+transformExpression (Old.Do i) =
+  New.Do (transformDo i)
+transformExpression (Old.ArrowE i) =
+  New.ArrowE (transformArrowExp i)
+transformExpression (Old.NamedTypeE i) =
+  New.NamedTypeE (transformNamedType i)
+transformExpression (Old.RefinedE i) =
+  New.RefinedE (transformTypeRefine i)
+transformExpression (Old.UniverseName i) =
+  New.UniverseName (transformUniverseExpression i)
+transformExpression (Old.Parened e) =
+  New.Parened (transformExpression e)
 
 --------------------------------------------------------------------------------
 -- Types
@@ -131,8 +172,8 @@ transformFunctionLike (Old.Like name args body) =
 transformModuleOpen :: Old.ModuleOpen -> New.ModuleOpen
 transformModuleOpen (Old.Open mod) = New.Open mod
 
-transformMoudleOpenExpr :: Old.ModuleOpenExpr -> New.ModuleOpenExpr
-transformMoudleOpenExpr (Old.OpenExpress modName expr) =
+transformModuleOpenExpr :: Old.ModuleOpenExpr -> New.ModuleOpenExpr
+transformModuleOpenExpr (Old.OpenExpress modName expr) =
   New.OpenExpress modName (transformExpression expr)
 
 transformArg :: Old.Arg -> New.Arg
@@ -176,8 +217,8 @@ transformString (Old.Sho t) = New.Sho t
 transformBlock :: Old.Block -> New.Block
 transformBlock (Old.Bloc expr) = New.Bloc (transformExpression expr)
 
-transfromLambda :: Old.Lambda -> New.Lambda
-transfromLambda (Old.Lamb args body) =
+transformLambda :: Old.Lambda -> New.Lambda
+transformLambda (Old.Lamb args body) =
   New.Lamb (transformMatchLogic <$> args) (transformExpression body)
 
 transformApplication :: Old.Application -> New.Application
