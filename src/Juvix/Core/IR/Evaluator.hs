c692163741cd8bc6e704137054a3bf4d65d485ff
Andy Morris <andy@cryptium.ch>
2020-06-19 15:36:29 +0200

    Fix bug in subst'

--- src/Juvix/Core/IR/Evaluator.hs
+++ src/Juvix/Core/IR/Evaluator.hs
@@ -114,7 +114,7 @@ instance
   subst' i e (IR.PrimTy' t a) =
     IR.PrimTy' t (subst' i e a)
   subst' i e (IR.Pi' π s t a) =
-    IR.Pi' π (subst' i e s) (subst' (succ i) (weak e) t) (subst' i e a)
+    IR.Pi' π (subst' i e s) (subst' (succ i) (weak' i e) t) (subst' i e a)
   subst' i e (IR.Lam' t a) =
     IR.Lam' (subst' (succ i) (weak e) t) (subst' i e a)
   subst' i e (IR.Let' l b a) =
