01d824fa6189d74e246a6daf4cf273ed587276b3
Christopher Goes <cwgoes@pluranimity.org>
2020-06-19 16:31:41 +0200

    Conversion, def todo

---
+++ src/Juvix/Core/ErasedAnn/Conversion.hs
@@ -0,0 +1,8 @@
+module Juvix.Core.ErasedAnn.Conversion where
+
+import qualified Juvix.Core.Erased.Types as E
+import Juvix.Core.ErasedAnn.Types
+import Juvix.Library
+
+convert :: E.Term primTy primVal -> Term primTy primVal
+convert = undefined
