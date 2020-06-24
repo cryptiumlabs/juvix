1fedc2c5ca6282daac09828957fa69d7ff00d858
April Gonçalves <april@cyberglot.me>
2020-06-24 18:20:48 +0100

    Add context passes: infix precedence

---
+++ src/Juvix/FrontendDesugar/ContextPasses/InfixPrecedence/Extend.hs
@@ -0,0 +1,13 @@
+module Juvix.FrontendDesugar.ContextPasses.InfixPrecedence.Extend
+  ( module Juvix.FrontendDesugar.RemoveDo.Extend,
+    module Juvix.FrontendDesugar.ContextPasses.InfixPrecedence.Extend,
+  )
+where
+
+import Juvix.Frontend.Types.Base
+import Juvix.FrontendDesugar.RemoveDo.Extend hiding (extendExpression, extendInfix)
+import qualified Juvix.FrontendDesugar.RemoveDo.Extend as Ext
+import Juvix.Library
+
+extendExpression :: ExtExpression
+extendExpression = Ext.extendExpression {typeInfix = Nothing}
