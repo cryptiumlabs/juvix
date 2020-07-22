5840a7880b754789ca6a7c6df1d8b3505eb95a9b
mariari <mariari@protonmail.com>
2020-07-22 09:14:29 -0400

    Added in the boilerplate for types, environment, and extensions

---
+++ src/Juvix/FrontendContextualise/InfixPrecedence/Extend.hs
@@ -0,0 +1,13 @@
+module Juvix.FrontendContextualise.InfixPrecedence.Extend
+  ( module Juvix.FrontendDesugar.RemoveDo.Extend,
+    module Juvix.FrontendContextualise.InfixPrecedence.Extend,
+  )
+where
+
+import Juvix.Frontend.Types.Base
+import Juvix.FrontendDesugar.RemoveDo.Extend hiding (extendExpression)
+import qualified Juvix.FrontendDesugar.RemoveDo.Extend as Ext
+import Juvix.Library
+
+extendExpression :: ExtExpression
+extendExpression = Ext.extendExpression {typeInfix = Nothing}
