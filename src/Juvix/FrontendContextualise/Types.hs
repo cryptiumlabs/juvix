1151f850e9dd8038002ff16554ca9f58d06d15bd
Marty Stumpf <thealmartyblog@gmail.com>
2020-07-22 10:13:14 -0700

    Remove changes for another pass.

--- src/Juvix/FrontendContextualise/Types.hs
+++
@@ -1,7 +0,0 @@
-module Juvix.FrontendContextualise.Types
-  ( module Juvix.FrontendContextualise.InfixPrecedence.Types,
-  )
-where
-
--- just import the last contextual pass here and export it
-import Juvix.FrontendContextualise.InfixPrecedence.Types
