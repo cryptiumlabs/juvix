6145b4b8966c727ee0ce72f3b4aa8d3fb07ca576
Marty Stumpf <thealmartyblog@gmail.com>
2020-07-27 09:59:41 -0700

    Clean up warnings.

--- src/Juvix/Core/Erased/Extend.hs
+++ src/Juvix/Core/Erased/Extend.hs
@@ -1,26 +1,38 @@
 module Juvix.Core.Erased.Extend where
 
 import qualified Juvix.Core.HRAnn.Extend as HR
-import qualified Juvix.Core.IR.Types as IR
 import Juvix.Core.IR.Types.Base
-import Juvix.Library
 
+-- extTerm :: template-haskell-2.15.0.0:Language.Haskell.TH.Lib.Internal.TypeQ
+--                  -> template-haskell-2.15.0.0:Language.Haskell.TH.Lib.Internal.TypeQ
+--                  -> ExtTerm
 extTerm = HR.extTerm
 
+-- extTerm :: template-haskell-2.15.0.0:Language.Haskell.TH.Lib.Internal.TypeQ
+--                  -> template-haskell-2.15.0.0:Language.Haskell.TH.Lib.Internal.TypeQ
+-- -> ExtElim
 extElim = HR.extElim
 
+extValue :: p1 -> p2 -> ExtValue
 extValue = \_ _ -> defaultExtValue
 
+extNeutral :: p1 -> p2 -> ExtNeutral
 extNeutral = \_ _ -> defaultExtNeutral
 
+extDatatype :: p1 -> p2 -> ExtDatatype
 extDatatype = \_ _ -> defaultExtDatatype
 
+extDataArg :: p1 -> p2 -> ExtDataArg
 extDataArg = \_ _ -> defaultExtDataArg
 
+extDataCon :: p1 -> p2 -> ExtDataCon
 extDataCon = \_ _ -> defaultExtDataCon
 
+extFunction :: p1 -> p2 -> ExtFunction
 extFunction = \_ _ -> defaultExtFunction
 
+extFunClause :: p1 -> p2 -> ExtFunClause
 extFunClause = \_ _ -> defaultExtFunClause
 
+extPattern :: p1 -> p2 -> ExtPattern
 extPattern = \_ _ -> defaultExtPattern
