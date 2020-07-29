1ac8e18b1dc549cc1f93265f1d3642ed4ffecc6c
Marty Stumpf <thealmartyblog@gmail.com>
2020-07-29 08:59:55 -0700

    Fix type sigs and apply hlint suggestions.

--- src/Juvix/Core/Erased/Extend.hs
+++ src/Juvix/Core/Erased/Extend.hs
@@ -1,38 +1,35 @@
 module Juvix.Core.Erased.Extend where
 
+import Extensible (TypeQ)
 import qualified Juvix.Core.HRAnn.Extend as HR
 import Juvix.Core.IR.Types.Base
 
--- extTerm :: template-haskell-2.15.0.0:Language.Haskell.TH.Lib.Internal.TypeQ
---                  -> template-haskell-2.15.0.0:Language.Haskell.TH.Lib.Internal.TypeQ
---                  -> ExtTerm
+extTerm :: TypeQ -> TypeQ -> ExtTerm
 extTerm = HR.extTerm
 
--- extTerm :: template-haskell-2.15.0.0:Language.Haskell.TH.Lib.Internal.TypeQ
---                  -> template-haskell-2.15.0.0:Language.Haskell.TH.Lib.Internal.TypeQ
--- -> ExtElim
+extElim :: TypeQ -> TypeQ -> ExtElim
 extElim = HR.extElim
 
 extValue :: p1 -> p2 -> ExtValue
-extValue = \_ _ -> defaultExtValue
+extValue _ _ = defaultExtValue
 
 extNeutral :: p1 -> p2 -> ExtNeutral
-extNeutral = \_ _ -> defaultExtNeutral
+extNeutral _ _ = defaultExtNeutral
 
 extDatatype :: p1 -> p2 -> ExtDatatype
-extDatatype = \_ _ -> defaultExtDatatype
+extDatatype _ _ = defaultExtDatatype
 
 extDataArg :: p1 -> p2 -> ExtDataArg
-extDataArg = \_ _ -> defaultExtDataArg
+extDataArg _ _ = defaultExtDataArg
 
 extDataCon :: p1 -> p2 -> ExtDataCon
-extDataCon = \_ _ -> defaultExtDataCon
+extDataCon _ _ = defaultExtDataCon
 
 extFunction :: p1 -> p2 -> ExtFunction
-extFunction = \_ _ -> defaultExtFunction
+extFunction _ _ = defaultExtFunction
 
 extFunClause :: p1 -> p2 -> ExtFunClause
-extFunClause = \_ _ -> defaultExtFunClause
+extFunClause _ _ = defaultExtFunClause
 
 extPattern :: p1 -> p2 -> ExtPattern
-extPattern = \_ _ -> defaultExtPattern
+extPattern _ _ = defaultExtPattern
