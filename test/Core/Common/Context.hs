8f0f189a811944d82edec2e16a700bee224680e7
mariari <mariari@protonmail.com>
2020-08-12 13:55:59 -0400

    added another test and deleted unuused tests

--- test/Core/Common/Context.hs
+++ test/Core/Common/Context.hs
@@ -7,57 +7,8 @@ import Juvix.Library
 import qualified Test.Tasty as T
 import qualified Test.Tasty.HUnit as T
 
--- nestedRecord :: Context.T term ty sumRep
--- nestedRecord =
---   Context.fromList
---     [ ( "foo",
---         Context.Record
---           (Context.fromList [("a", Context.Record (Context.T mempty) Nothing)])
---           Nothing
---       )
---     ]
-
--- memptyTest :: Context.Definition Natural Natural Natural
--- memptyTest =
---   foldr
---     const
---     (Context.Def Nothing Nothing 1 Context.default')
---     (fmap identity (Context.fromList []))
-
--- lookupTest :: Maybe (Context.Definition term ty sumRep)
--- lookupTest = Context.lookup "foo.a" nestedRecord
-
 foo :: Context.T Int Int Int
 foo = Context.empty ("Foo" :| ["Bar", "Baz"])
-
-foo' =
-  Context.addPathWithValue
-    ("Foo" :| ["Bar", "Baz", "Barry"])
-    (Context.Unknown Nothing)
-    foo
-
-foo'' = Context.switchNameSpace ("Foo" :| ["Bar", "Baz", "Barry"]) foo
-
-foo''' = Context.switchNameSpace ("Foo" :| ["Bar", "Min"]) foo
-
-foo'''' = Context.switchNameSpace ("Foo" :| ["Bar", "Baz"]) foo
-
-barTest =
-  let Right foo = foo'
-   in Context.switchNameSpace ("Foo" :| ["Bar", "Baz", "Barr"]) foo
-
-barTest' =
-  let Right bar = barTest
-   in Context.removeNameSpace ("Foo" :| ["Bar", "Baz", "Barry"]) bar
-
-testInner =
-  let Right foo = foo'
-   in Context.switchNameSpace ("TopLevel" :| ["Foo"]) foo
-
-switchTest =
-  let Right foo = foo'
-      Right foo'' = Context.switchNameSpace ("TopLevel" :| ["Bar"]) foo
-   in Context.switchNameSpace ("TopLevel" :| ["Foo"]) foo''
 
 contextTests :: T.TestTree
 contextTests =
@@ -69,7 +20,8 @@ contextTests =
       checkCorrectResolution,
       privateFromAbove,
       privateBeatsPublic,
-      localBeatsGlobal
+      localBeatsGlobal,
+      nonRelatedModuleStillPersists
     ]
 
 switchAboveLookupCheck :: T.TestTree
@@ -223,3 +175,16 @@ localBeatsGlobal =
         |> Just
         |> (looked T.@=?)
         |> T.testCase "public beats global"
+
+nonRelatedModuleStillPersists :: T.TestTree
+nonRelatedModuleStillPersists =
+  let Right topBar = Context.switchNameSpace ("TopLevel" :| ["Bar"]) foo
+      Right food = Context.switchNameSpace ("TopLevel" :| ["Foo"]) topBar
+      --
+      looked = food Context.!? (Context.topLevelName :| ["Bar"])
+      --
+      isOutSideRec (Just (Context.Outside (Context.Record _ _))) = True
+      isOutSideRec _ = False
+   in T.testCase
+        "differnet module persists through switch"
+        (isOutSideRec looked T.@=? True)
