4c081935b5e7ffc67fe8c2d5bfd74cca54cae453
mariari <mariari@protonmail.com>
2020-08-31 15:22:38 -0400

    started tests

---
+++ test/FrontendContextualise/Module/Open.hs
@@ -0,0 +1,30 @@
+module FrontendContextualise.Module.Open where
+
+import Juvix.Library
+import qualified Juvix.FrontendContextualise.ModuleOpen.Environment as Env
+import qualified Data.Text as Text
+import qualified Juvix.Core.Common.Context as Context
+import qualified Juvix.Core.Common.NameSpace as NameSpace
+import qualified Juvix.Library.HashMap as HashMap
+-- import qualified Test.Tasty as T
+-- import qualified Test.Tasty.HUnit as T
+
+
+prelude =
+  Context.empty (pure "Prelude")
+  |> Context.add
+      (NameSpace.Pub "->")
+      (Context.Def Nothing Nothing 3 (Context.Pred Context.Right 0))
+  |> Context.add
+      (NameSpace.Pub ":")
+      (Context.Def Nothing Nothing 1 (Context.Pred Context.Right 2))
+
+
+
+ourModule =
+  case Context.switchNameSpace (Context.topLevelName :| ["Londo"]) prelude of
+    Right ctx -> ctx
+    Left _ -> prelude
+
+firstOpen =
+  Env.resolve ourModule [Env.Pre [pure "Prelude"] [] (Context.topLevelName :| ["Londo"])]
