2a6523977528cadf6b347dd37198188d2a0408b5
Marty Stumpf <thealmartyblog@gmail.com>
2020-09-16 11:35:12 -0700

    Finish updating parser tests.

--- test/Frontend.hs
+++ test/Frontend.hs
@@ -19,22 +19,22 @@ allParserTests :: T.TestTree
 allParserTests =
   T.testGroup
     "Parser Tests"
-    [ -- many1FunctionsParser,
+    [ many1FunctionsParser,
       sigTest1,
       sigTest2,
       fun1,
       fun2,
       sumTypeTest,
-      -- superArrowCase,
+      superArrowCase,
       typeTest,
       moduleOpen,
       moduleOpen',
-      -- typeNameNoUniverse,
-      -- simpleNamedCon,
-      -- matchMoreComplex,
-      -- condTest1,
-      -- record1,
-      -- parens1,
+      typeNameNoUniverse,
+      simpleNamedCon,
+      matchMoreComplex,
+      condTest1,
+      record1,
+      parens1,
       -- pre-processor tests
       removeNoComment,
       removeNewLineBefore,
@@ -57,10 +57,6 @@ test =
     \= 3 let foo = 3 let foo = 3 let foo = 3 let foo = 3 let foo = 3 let foo = 3 let \
     \foo = 3 let foo = 3 let foo = 3 let foo = 3 "
 
-isFail :: Result a -> Bool
-isFail (Done _ _) = False
-isFail _ = True
-
 takeResult :: Result s -> s
 takeResult (Done _ x) = x
 takeResult (Partial y) = takeResult (y "")
@@ -71,8 +67,7 @@ shouldParseAs ::
 shouldParseAs name parses x y =
   T.testGroup
     "parse Test"
-    [ T.testCase (name <> " parser failed") (isFail (parses x) T.@=? True),
-      T.testCase
+    [ T.testCase
         ("parse: " <> name <> " " <> show x <> " should parse to " <> show y)
         (takeResult (parses x) T.@=? y)
     ]
@@ -205,62 +200,26 @@ token = goldenTest "Token" "test/examples/Token.ju"
 -- Parse Many at once
 --------------------------------------------------------------------------------
 
--- many1FunctionsParser :: T.TestTree
--- many1FunctionsParser =
---   shouldParseAs
---     "many1FunctionsParser"
---     (many Parser.topLevelSN)
---     ( "let foo a b c = (+) (a + b) c"
---         <> "let bah = foo 1 2 3"
---         <> "let nah "
---         <> "  | bah == 5 = 7 "
---         <> "  | else     = 11"
---         <> "let test = "
---         <> "  let check = nah in "
---         <> "  case check of "
---         <> "  | seven -> 11 "
---         <> "  | eleven -> 7 "
---         <> "  | f  -> open Fails in "
---         <> "          print failed; "
---         <> "          fail"
---     )
---     [Function' (Func' (Like' {functionLikedName = foo, functionLikeArgs = [ConcreteA'
---     (MatchLogic' {matchLogicContents = MatchName' a (), matchLogicNamed = Nothing,
---     annMatchLogic = ()}) (),ConcreteA' (MatchLogic' {matchLogicContents = MatchName'
---     b (), matchLogicNamed = Nothing, annMatchLogic = ()}) (),ConcreteA' (MatchLogic'
---     {matchLogicContents = MatchName' c (), matchLogicNamed = Nothing, annMatchLogic
---     = ()}) ()], functionLikeBody = Body' (Application' (App' {applicationName =
---     Name' (+ :| []) (), applicationArgs = Parened' (Infix' (Inf' {infixLeft = Name'
---     (a :| []) (), infixOp = + :| [], infixRight = Name' (b :| []) (), annInf = ()})
---     ()) () :| [Name' (c :| []) ()], annApp = ()}) ()) (), annLike = ()}) ()) (),Function'
---     (Func' (Like' {functionLikedName = bah, functionLikeArgs = [], functionLikeBody
---     = Body' (Application' (App' {applicationName = Name' (foo :| []) (), applicationArgs
---     = Constant' (Number' (Integer'' 1 ()) ()) () :| [Constant' (Number' (Integer''
---     2 ()) ()) (),Constant' (Number' (Integer'' 3 ()) ()) ()], annApp = ()}) ())
---     (), annLike = ()}) ()) (),Function' (Func' (Like' {functionLikedName = nah,
---     functionLikeArgs = [], functionLikeBody = Guard' (C' (CondExpression' {condLogicPred
---     = Infix' (Inf' {infixLeft = Name' (bah :| []) (), infixOp = == :| [], infixRight
---     = Constant' (Number' (Integer'' 5 ()) ()) (), annInf = ()}) (), condLogicBody
---     = Constant' (Number' (Integer'' 7 ()) ()) (), annCondExpression = ()} :| [CondExpression'
---     {condLogicPred = Name' (else :| []) (), condLogicBody = Constant' (Number' (Integer''
---     11 ()) ()) (), annCondExpression = ()}]) ()) (), annLike = ()}) ()) (),Function'
---     (Func' (Like' {functionLikedName = test, functionLikeArgs = [], functionLikeBody
---     = Body' (Let' (Let''' {letBindings = Like' {functionLikedName = check, functionLikeArgs
---     = [], functionLikeBody = Body' (Name' (nah :| []) ()) (), annLike = ()}, letBody
---     = Match' (Match''' {matchOn = Name' (check :| []) (), matchBindigns = MatchL'
---     {matchLPattern = MatchLogic' {matchLogicContents = MatchName' seven (), matchLogicNamed
---     = Nothing, annMatchLogic = ()}, matchLBody = Constant' (Number' (Integer'' 11
---     ()) ()) (), annMatchL = ()} :| [MatchL' {matchLPattern = MatchLogic' {matchLogicContents
---     = MatchName' eleven (), matchLogicNamed = Nothing, annMatchLogic = ()}, matchLBody
---     = Constant' (Number' (Integer'' 7 ()) ()) (), annMatchL = ()},MatchL' {matchLPattern
---     = MatchLogic' {matchLogicContents = MatchName' f (), matchLogicNamed = Nothing,
---     annMatchLogic = ()}, matchLBody = OpenExpr' (OpenExpress' {moduleOpenExprModuleN
---     = Fails :| [], moduleOpenExprExpr = Do' (Do''' (DoBody' {doBodyName = Nothing,
---     doBodyExpr = Application' (App' {applicationName = Name' (print :| []) (), applicationArgs
---     = Name' (failed :| []) () :| [], annApp = ()}) (), annDoBody = ()} :| [DoBody'
---     {doBodyName = Nothing, doBodyExpr = Name' (fail :| []) (), annDoBody = ()}])
---     ()) (), annOpenExpress = ()}) (), annMatchL = ()}], annMatch'' = ()}) (), annLet''
---     = ()}) ()) (), annLike = ()}) ()) ()]
+many1FunctionsParser :: T.TestTree
+many1FunctionsParser =
+  shouldParseAs
+    "many1FunctionsParser"
+    (parse $ many Parser.topLevelSN)
+    ( "let foo a b c = (+) (a + b) c"
+        <> "let bah = foo 1 2 3"
+        <> "let nah "
+        <> "  | bah == 5 = 7 "
+        <> "  | else     = 11"
+        <> "let test = "
+        <> "  let check = nah in "
+        <> "  case check of "
+        <> "  | seven -> 11 "
+        <> "  | eleven -> 7 "
+        <> "  | f  -> open Fails in "
+        <> "          print failed; "
+        <> "          fail"
+    )
+    [Function' (Func' (Like' {functionLikedName = Sym "foo", functionLikeArgs = [ConcreteA' (MatchLogic' {matchLogicContents = MatchName' (Sym "a") (), matchLogicNamed = Nothing, annMatchLogic = ()}) (), ConcreteA' (MatchLogic' {matchLogicContents = MatchName' (Sym "b") (), matchLogicNamed = Nothing, annMatchLogic = ()}) (), ConcreteA' (MatchLogic' {matchLogicContents = MatchName' (Sym "c") (), matchLogicNamed = Nothing, annMatchLogic = ()}) ()], functionLikeBody = Body' (Application' (App' {applicationName = Name' (Sym "+" :| []) (), applicationArgs = Parened' (Infix' (Inf' {infixLeft = Name' (Sym "a" :| []) (), infixOp = Sym "+" :| [], infixRight = Name' (Sym "b" :| []) (), annInf = ()}) ()) () :| [Name' (Sym "clet" :| []) (), Name' (Sym "bah" :| []) ()], annApp = ()}) ()) (), annLike = ()}) ()) ()]
 
 --------------------------------------------------------------------------------
 -- Sig Test
@@ -326,27 +285,13 @@ sumTypeTest =
 -- Arrow Testing
 --------------------------------------------------
 
--- superArrowCase :: T.TestTree
--- superArrowCase =
---   shouldParseAs
---     "superArrowCase"
---     (parse Parser.expression)
---     "( b : Bah ->  c : B -o Foo) -> Foo a b -> a : Bah a c -o ( HAHAHHA -> foo )"
---     "Infix' (Inf' {infixLeft = Parened' (Infix' (Inf' {infixLeft = Name' (b :| [])
---     (), infixOp = : :| [], infixRight = Infix' (Inf' {infixLeft = Name' (Bah :|
---     []) (), infixOp = -> :| [], infixRight = Infix' (Inf' {infixLeft = Name' (c
---     :| []) (), infixOp = : :| [], infixRight = Infix' (Inf' {infixLeft = Name' (B
---     :| []) (), infixOp = -o :| [], infixRight = Name' (Foo :| []) (), annInf = ()})
---     (), annInf = ()}) (), annInf = ()}) (), annInf = ()}) ()) (), infixOp = -> :|
---     [], infixRight = Infix' (Inf' {infixLeft = Application' (App' {applicationName
---     = Name' (Foo :| []) (), applicationArgs = Name' (a :| []) () :| [Name' (b :|
---     []) ()], annApp = ()}) (), infixOp = -> :| [], infixRight = Infix' (Inf' {infixLeft
---     = Name' (a :| []) (), infixOp = : :| [], infixRight = Infix' (Inf' {infixLeft
---     = Application' (App' {applicationName = Name' (Bah :| []) (), applicationArgs
---     = Name' (a :| []) () :| [Name' (c :| []) ()], annApp = ()}) (), infixOp = -o
---     :| [], infixRight = Parened' (Infix' (Inf' {infixLeft = Name' (HAHAHHA :| [])
---     (), infixOp = -> :| [], infixRight = Name' (foo :| []) (), annInf = ()}) ())
---     (), annInf = ()}) (), annInf = ()}) (), annInf = ()}) (), annInf = ()}) ()"
+superArrowCase :: T.TestTree
+superArrowCase =
+  shouldParseAs
+    "superArrowCase"
+    (parse Parser.expression)
+    "( b : Bah ->  c : B -o Foo) -> Foo a b -> a : Bah a c -o ( HAHAHHA -> foo )"
+    (Infix' (Inf' {infixLeft = Parened' (Infix' (Inf' {infixLeft = Name' (Sym "b" :| []) (), infixOp = Sym ":" :| [], infixRight = Infix' (Inf' {infixLeft = Name' (Sym "Bah" :| []) (), infixOp = Sym "->" :| [], infixRight = Infix' (Inf' {infixLeft = Name' (Sym "c" :| []) (), infixOp = Sym ":" :| [], infixRight = Infix' (Inf' {infixLeft = Name' (Sym "B" :| []) (), infixOp = Sym "-o" :| [], infixRight = Name' (Sym "Foo" :| []) (), annInf = ()}) (), annInf = ()}) (), annInf = ()}) (), annInf = ()}) ()) (), infixOp = Sym "->" :| [], infixRight = Infix' (Inf' {infixLeft = Application' (App' {applicationName = Name' (Sym "Foo" :| []) (), applicationArgs = Name' (Sym "a" :| []) () :| [Name' (Sym "b" :| []) ()], annApp = ()}) (), infixOp = Sym "->" :| [], infixRight = Infix' (Inf' {infixLeft = Name' (Sym "a" :| []) (), infixOp = Sym ":" :| [], infixRight = Infix' (Inf' {infixLeft = Application' (App' {applicationName = Name' (Sym "Bah" :| []) (), applicationArgs = Name' (Sym "a" :| []) () :| [Name' (Sym "c" :| []) ()], annApp = ()}) (), infixOp = Sym "-o" :| [], infixRight = Parened' (Infix' (Inf' {infixLeft = Name' (Sym "HAHAHHA" :| []) (), infixOp = Sym "->" :| [], infixRight = Name' (Sym "foo" :| []) (), annInf = ()}) ()) (), annInf = ()}) (), annInf = ()}) (), annInf = ()}) (), annInf = ()}) ())
 
 --------------------------------------------------
 -- alias tests
@@ -394,101 +339,76 @@ moduleOpen' =
     )
     [Module' (Mod' (Like' {functionLikedName = Sym "Bah", functionLikeArgs = [ConcreteA' (MatchLogic' {matchLogicContents = MatchCon' (Sym "M" :| []) [] (), matchLogicNamed = Nothing, annMatchLogic = ()}) ()], functionLikeBody = Body' (ModuleOpen' (Open' (Sym "M" :| []) ()) () :| [Signature' (Sig' {signatureName = Sym "bah", signatureUsage = Nothing, signatureArrowType = Name' (Sym "Rec" :| []) (), signatureConstraints = [], annSig = ()}) (), Function' (Func' (Like' {functionLikedName = Sym "bah", functionLikeArgs = [ConcreteA' (MatchLogic' {matchLogicContents = MatchName' (Sym "t") (), matchLogicNamed = Nothing, annMatchLogic = ()}) ()], functionLikeBody = Body' (ExpRecord' (ExpressionRecord' {expRecordFields = NonPunned' (Sym "a" :| []) (Infix' (Inf' {infixLeft = Name' (Sym "t" :| []) (), infixOp = Sym "+" :| [], infixRight = Constant' (Number' (Integer'' 3 ()) ()) (), annInf = ()}) ()) () :| [NonPunned' (Sym "b" :| []) (Application' (App' {applicationName = Name' (Sym "expr" :| []) (), applicationArgs = Name' (Sym "M" :| [Sym "N", Sym "t"]) () :| [], annApp = ()}) ()) ()], annExpressionRecord = ()}) ()) (), annLike = ()}) ()) ()]) (), annLike = ()}) ()) ()]
 
--- --------------------------------------------------
--- -- typeName tests
--- --------------------------------------------------
+--------------------------------------------------
+-- typeName tests
+--------------------------------------------------
 
--- typeNameNoUniverse :: T.TestTree
--- typeNameNoUniverse =
---   shouldParseAs
---     "typeNameNoUniverse"
---     Parser.expression
---     "Foo a b c (b -o d) a c u"
---     "Application' (App' {applicationName = Name' (Foo :| []) (), applicationArgs
---     = Name' (a :| []) () :| [Name' (b :| []) (),Name' (c :| []) (),Parened' (Infix'
---     (Inf' {infixLeft = Name' (b :| []) (), infixOp = -o :| [], infixRight = Name'
---     (d :| []) (), annInf = ()}) ()) (),Name' (a :| []) (),Name' (c :| []) (),Name'
---     (u :| []) ()], annApp = ()}) ()"
+typeNameNoUniverse :: T.TestTree
+typeNameNoUniverse =
+  shouldParseAs
+    "typeNameNoUniverse"
+    (parse Parser.expression)
+    "Foo a b c (b -o d) a c u"
+    (Application' (App' {applicationName = Name' (Sym "Foo" :| []) (), applicationArgs = Name' (Sym "a" :| []) () :| [Name' (Sym "b" :| []) (), Name' (Sym "c" :| []) (), Parened' (Infix' (Inf' {infixLeft = Name' (Sym "b" :| []) (), infixOp = Sym "-o" :| [], infixRight = Name' (Sym "d" :| []) (), annInf = ()}) ()) (), Name' (Sym "a" :| []) (), Name' (Sym "c" :| []) (), Name' (Sym "u" :| []) ()], annApp = ()}) ())
 
 -- --------------------------------------------------------------------------------
 -- -- Match tests
 -- --------------------------------------------------------------------------------
 
--- simpleNamedCon :: T.TestTree
--- simpleNamedCon =
---   shouldParseAs
---     "simpleNamedCon"
---     Parser.matchLogic
---     "foo@( Hi a b c )"
---     "MatchLogic' {matchLogicContents = MatchCon' (Hi :| []) [MatchLogic' {matchLogicContents
---     = MatchName' a (), matchLogicNamed = Nothing, annMatchLogic = ()},MatchLogic'
---     {matchLogicContents = MatchName' b (), matchLogicNamed = Nothing, annMatchLogic
---     = ()},MatchLogic' {matchLogicContents = MatchName' c (), matchLogicNamed = Nothing,
---     annMatchLogic = ()}] (), matchLogicNamed = Just foo, annMatchLogic =
---     ()}"
+simpleNamedCon :: T.TestTree
+simpleNamedCon =
+  shouldParseAs
+    "simpleNamedCon"
+    (parse Parser.matchLogic)
+    "foo@( Hi a b c )"
+    (MatchLogic' {matchLogicContents = MatchCon' (Sym "Hi" :| []) [MatchLogic' {matchLogicContents = MatchName' (Sym "a") (), matchLogicNamed = Nothing, annMatchLogic = ()}, MatchLogic' {matchLogicContents = MatchName' (Sym "b") (), matchLogicNamed = Nothing, annMatchLogic = ()}, MatchLogic' {matchLogicContents = MatchName' (Sym "c") (), matchLogicNamed = Nothing, annMatchLogic = ()}] (), matchLogicNamed = Just (Sym "foo"), annMatchLogic = ()})
 
--- matchMoreComplex :: T.TestTree
--- matchMoreComplex =
---   shouldParseAs
---     "matchMoreComplex"
---     Parser.matchLogic
---     "foo@( Hi nah@{ a = nah , f } b 5 )"
---     "MatchLogic' {matchLogicContents = MatchCon' (Hi :| []) [MatchLogic' {matchLogicContents
---     = MatchRecord' (NonPunned' (a :| []) (MatchLogic' {matchLogicContents = MatchName'
---     \nah (), matchLogicNamed = Nothing, annMatchLogic = ()}) () :| [Punned' (f :|
---     []) ()]) (), matchLogicNamed = Just nah, annMatchLogic = ()},MatchLogic'
---     {matchLogicContents = MatchName' b (), matchLogicNamed = Nothing, annMatchLogic
---     = ()},MatchLogic' {matchLogicContents = MatchConst' (Number' (Integer'' 5 ())
---     ()) (), matchLogicNamed = Nothing, annMatchLogic = ()}] (), matchLogicNamed
---     = Just foo, annMatchLogic = ()}"
+matchMoreComplex :: T.TestTree
+matchMoreComplex =
+  shouldParseAs
+    "matchMoreComplex"
+    (parse Parser.matchLogic)
+    "foo@( Hi nah@{ a = nah , f } b 5 )"
+    (MatchLogic' {matchLogicContents = MatchCon' (Sym "Hi" :| []) [MatchLogic' {matchLogicContents = MatchRecord' (NonPunned' (Sym "a" :| []) (MatchLogic' {matchLogicContents = MatchName' (Sym "nah") (), matchLogicNamed = Nothing, annMatchLogic = ()}) () :| [Punned' (Sym "f" :| []) ()]) (), matchLogicNamed = Just (Sym "nah"), annMatchLogic = ()}, MatchLogic' {matchLogicContents = MatchName' (Sym "b") (), matchLogicNamed = Nothing, annMatchLogic = ()}, MatchLogic' {matchLogicContents = MatchConst' (Number' (Integer'' 5 ()) ()) (), matchLogicNamed = Nothing, annMatchLogic = ()}] (), matchLogicNamed = Just (Sym "foo"), annMatchLogic = ()})
 
--- --------------------------------------------------------------------------------
--- -- Expression
--- --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Expression
+--------------------------------------------------------------------------------
 
--- condTest1 :: T.TestTree
--- condTest1 =
---   shouldParseAs
---     "condTest1"
---     Parser.cond
---     ( ""
---         <> "if  | foo  = a"
---         <> "    | else = b "
---     )
---     "C' (CondExpression' {condLogicPred = Name' (foo :| []) (), condLogicBody = Name'
---     (a :| []) (), annCondExpression = ()} :| [CondExpression' {condLogicPred = Name'
---     (else :| []) (), condLogicBody = Name' (b :| []) (), annCondExpression = ()}]) ()"
+condTest1 :: T.TestTree
+condTest1 =
+  shouldParseAs
+    "condTest1"
+    (parse Parser.cond)
+    ( ""
+        <> "if  | foo  = a"
+        <> "    | else = b "
+    )
+    (C' (CondExpression' {condLogicPred = Name' (Sym "foo" :| []) (), condLogicBody = Name' (Sym "a" :| []) (), annCondExpression = ()} :| [CondExpression' {condLogicPred = Name' (Sym "else" :| []) (), condLogicBody = Name' (Sym "b" :| []) (), annCondExpression = ()}]) ())
 
 -- --------------------------------------------------
 -- -- Record
 -- --------------------------------------------------
 
--- record1 :: T.TestTree
--- record1 =
---   shouldParseAs
---     "record1"
---     Parser.expression
---     "{a, b = 3+5}"
---     "ExpRecord' (ExpressionRecord' {expRecordFields = Punned' (a :| []) () :| [NonPunned'
---     (b :| []) (Infix' (Inf' {infixLeft = Constant' (Number' (Integer'' 3 ()) ())
---     (), infixOp = + :| [], infixRight = Constant' (Number' (Integer'' 5 ()) ())
---     (), annInf = ()}) ()) ()], annExpressionRecord = ()}) ()"
+record1 :: T.TestTree
+record1 =
+  shouldParseAs
+    "record1"
+    (parse Parser.expression)
+    "{a, b = 3+5}"
+    (ExpRecord' (ExpressionRecord' {expRecordFields = Punned' (Sym "a" :| []) () :| [NonPunned' (Sym "b" :| []) (Infix' (Inf' {infixLeft = Constant' (Number' (Integer'' 3 ()) ()) (), infixOp = Sym "+" :| [], infixRight = Constant' (Number' (Integer'' 5 ()) ()) (), annInf = ()}) ()) ()], annExpressionRecord = ()}) ())
 
 -- --------------------------------------------------
 -- -- parens
 -- --------------------------------------------------
 
--- parens1 :: T.TestTree
--- parens1 =
---   shouldParseAs
---     "parens1"
---     Parser.expression
---     "(       ( (({a, b = 3+5}))))"
---     "Parened' (Parened' (Parened' (Parened' (ExpRecord' (ExpressionRecord' {expRecordFields
---     = Punned' (a :| []) () :| [NonPunned' (b :| []) (Infix' (Inf' {infixLeft = Constant'
---     (Number' (Integer'' 3 ()) ()) (), infixOp = + :| [], infixRight = Constant'
---     (Number' (Integer'' 5 ()) ()) (), annInf = ()}) ()) ()], annExpressionRecord
---     = ()}) ()) ()) ()) ()) ()"
+parens1 :: T.TestTree
+parens1 =
+  shouldParseAs
+    "parens1"
+    (parse Parser.expression)
+    "(       ( (({a, b = 3+5}))))"
+    (Parened' (Parened' (Parened' (Parened' (ExpRecord' (ExpressionRecord' {expRecordFields = Punned' (Sym "a" :| []) () :| [NonPunned' (Sym "b" :| []) (Infix' (Inf' {infixLeft = Constant' (Number' (Integer'' 3 ()) ()) (), infixOp = Sym "+" :| [], infixRight = Constant' (Number' (Integer'' 5 ()) ()) (), annInf = ()}) ()) ()], annExpressionRecord = ()}) ()) ()) ()) ()) ())
 
 --------------------------------------------------------------------------------
 -- Spacer tests
