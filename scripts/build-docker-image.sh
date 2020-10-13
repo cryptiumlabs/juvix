24d4f1f3db79ecf5dd44c118fd2df652b15aefeb
Christopher Goes <cwgoes@pluranimity.org>
2020-10-13 12:44:32 +0200

    No SHM ..

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -6,7 +6,7 @@ set -xe
 
 time docker build -t $TAG .
 docker save $TAG > /tmp/image.tar
-TMPDIR=/var/run/shm docker-squash -i /tmp/image.tar -o /tmp/squashed.tar -t $TAG
+docker-squash -i /tmp/image.tar -o /tmp/squashed.tar -t $TAG
 rm /tmp/image.tar
 cat /tmp/squashed.tar | docker load
 rm /tmp/squashed.tar
