e38e2dd13dc33e4f367810280c267fc2dae4fbeb
Christopher Goes <cwgoes@pluranimity.org>
2020-10-13 12:25:37 +0200

    Use SHM

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+TAG=cryptiumlabs/juvix-ci
+
 set -xe
 
-time docker build -t cryptiumlabs/juvix-ci .
-docker save cryptiumlabs/juvix-ci > /tmp/image.tar
-docker-squash -i /tmp/image.tar -o /tmp/squashed.tar -t cryptiumlabs/juvix-ci
-cat /tmp/squashed.tar | docker load
-docker push cryptiumlabs/juvix-ci
+time docker build -t $TAG .
+docker save $TAG > /var/run/shm/image.tar
+TMPDIR=/var/run/shm docker-squash -i /var/run/shm/image.tar -o /var/run/shm/squashed.tar -t $TAG
+rm /var/run/shm/image.tar
+rm /var/run/shm/squashed.tar
+cat /var/run/shm/squashed.tar | docker load
+docker push $TAG
