f06da3ea4f26ca041d6626bfc63504f7deb349f8
Christopher Goes <cwgoes@pluranimity.org>
2020-10-13 12:02:57 +0200

    Update scripts

--- scripts/build-roswell.sh
+++ scripts/build-roswell.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-curl -sOL `curl -s https://api.github.com/repos/roswell/roswell/releases/latest | jq -r '.assets | .[] | select(.name|test("deb$")) | .browser_download_url'` --output roswell.deb
-dpkg -i roswell.deb
+curl -sOL `curl -s https://api.github.com/repos/roswell/roswell/releases/latest | jq -r '.assets | .[] | select(.name|test("deb$")) | .browser_download_url'`
+dpkg -i roswell*.deb
 export PATH=~/.roswell/bin:$PATH
 ros install metastatedev/org-generation
