a2a13a467a905fef10d6dc95be114fe39cf0d15b
Christopher Goes <cwgoes@pluranimity.org>
2020-10-13 12:19:45 +0200

    Update build script

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -2,5 +2,8 @@
 
 set -xe
 
-time docker build --squash -t cryptiumlabs/juvix-ci .
+time docker build -t cryptiumlabs/juvix-ci .
+docker save cryptiumlabs/juvix-ci > /tmp/image.tar
+docker-squash -i /tmp/image.tar -o /tmp/squashed.tar -t cryptiumlabs/juvix-ci
+cat /tmp/squashed.tar | docker load
 docker push cryptiumlabs/juvix-ci
