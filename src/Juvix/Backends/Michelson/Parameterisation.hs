6b589dbd8e0e456db0ca09208a9e867dd7c039c1
mariari <mariari@protonmail.com>
2020-10-15 10:31:00 -0400

    forgot ,

--- src/Juvix/Backends/Michelson/Parameterisation.hs
+++ src/Juvix/Backends/Michelson/Parameterisation.hs
@@ -224,7 +224,7 @@ builtinValues =
     ("Michelson.fail-with", Inst M.FAILWITH),
     ("Michelson.self", Inst (M.SELF "" "")),
     ("Michelson.unit", Inst (M.UNIT "" "")),
-      -- added symbols to not take values
+    -- added symbols to not take values
     ("Michelson.if", Inst (M.IF [] []))
   ]
     |> fmap (first NameSymbol.fromSymbol)
