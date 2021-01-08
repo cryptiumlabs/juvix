4ff575b028138e277770951d9cf68f465586d416
Christopher Goes <cwgoes@pluranimity.org>
2021-01-08 13:03:45 -0800

    Test second module

--- app/Compile.hs
+++ app/Compile.hs
@@ -29,7 +29,8 @@ import Types
 parse :: FilePath -> IO FE.FinalContext
 parse fin = do
   --core <- Pipeline.toCore ["stdlib/Prelude.ju", "stdlib/Michelson.ju", "stdlib/MichelsonAlias.ju", fin]
-  core <- Pipeline.toCore [fin]
+  --core <- Pipeline.toCore [fin]
+  core <- Pipeline.toCore ["test/examples/demo/helper.ju", fin]
   case core of
     Right ctx -> pure ctx
     Left err -> do
