1956934fdc01597737e0bf142203810f3f1de690
Fraccaroli Gianmarco <gianmarco.fraccaroli@gmail.com>
2021-02-15 15:21:16 +0100

    update drone.yml and related files

--- library/Translate/test/Contextualise/Contextify.hs
+++ library/Translate/test/Contextualise/Contextify.hs
@@ -2,14 +2,12 @@
 
 module Contextualise.Contextify where
 
-import qualified Data.List.NonEmpty as NonEmpty
 import qualified Juvix.Core.Common.Context as Context
 import qualified Juvix.Frontend.Parser as Parser
 import qualified Juvix.Frontend.Types as AST
 import qualified Juvix.FrontendContextualise as Contextualize
 import qualified Juvix.FrontendDesugar as Desugar
 import Juvix.Library
-import qualified Juvix.Library.NameSymbol as NameSymbol
 import qualified Test.Tasty as T
 import qualified Test.Tasty.HUnit as T
 
@@ -21,7 +19,7 @@ top :: T.TestTree
 top =
   T.testGroup
     "contextify tests:"
-    [infixPlaceTest]
+    [infixPlaceTest, sumConTest]
 
 --------------------------------------------------------------------------------
 -- tests
@@ -29,14 +27,34 @@ top =
 
 infixPlaceTest :: T.TestTree
 infixPlaceTest =
-  ctx Context.!? "+"
-    |> fmap (Context.precedence . Context.extractValue)
-    |> (T.@=? Just (Context.Pred Context.Left 5))
-    |> T.testCase
-      "infix properly adds precedence"
+  ( do
+      Right (ctx, _) <-
+        Contextualize.contextify (("Foo", desugared) :| [])
+      ctx Context.!? "+"
+        |> fmap ((\(Context.Def d) -> Context.defPrecedence d) . Context.extractValue)
+        |> (T.@=? Just (Context.Pred Context.Left 5))
+      |> T.testCase
+        "infix properly adds precedence"
+  )
   where
-    Right (ctx, _) =
-      Contextualize.contextify (("Foo", desugared) :| [])
     Right desugared =
       Desugar.op . AST.extractTopLevel
         <$> Parser.parseOnly "let (+) = 3 declare infixl (+) 5"
+
+sumConTest :: T.TestTree
+sumConTest =
+  T.testGroup
+    "Sum Constructors are properly added:"
+    [ T.testCase "Bool properly adds True" (test "True"),
+      T.testCase "Bool properly adds False" (test "False")
+    ]
+  where
+    test str = do
+      Right (ctx, _) <-
+        Contextualize.contextify (("Foo", desugared) :| [])
+      ctx Context.!? str
+        |> fmap Context.extractValue
+        |> (T.@=? Just (Context.SumCon (Context.Sum Nothing "bool")))
+    Right desugared =
+      Desugar.op . AST.extractTopLevel
+        <$> Parser.parseOnly "type bool = True | False"
