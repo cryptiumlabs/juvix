b76221e8f98c84bc8d1eee17738c63452903f1a6
Marty Stumpf <thealmartyblog@gmail.com>
2021-02-22 08:15:09 -0800

    Remove unused import comments.

--- library/Core/src/Juvix/Core/IR/Typechecker.hs
+++ library/Core/src/Juvix/Core/IR/Typechecker.hs
@@ -10,19 +10,11 @@ module Juvix.Core.IR.Typechecker
   )
 where
 
--- import qualified Data.IntMap.Strict as IntMap
--- import Data.List.NonEmpty ((<|))
--- import qualified Juvix.Core.Application as App
--- import qualified Juvix.Core.IR.CheckTerm as IR
--- import qualified Juvix.Core.IR.Evaluator as Eval
 import Juvix.Core.IR.Typechecker.Env as Env
 import Juvix.Core.IR.Typechecker.Types as Typed
 import qualified Juvix.Core.IR.Types as IR
 import qualified Juvix.Core.IR.Types.Base as IR
--- import qualified Juvix.Core.Parameterisation as Param
 import Juvix.Library hiding (Datatype)
-
--- import qualified Juvix.Library.Usage as Usage
 
 typeCheckDeclaration ::
   [IR.RawDatatype' ext0 primTy0 primVal0] ->
