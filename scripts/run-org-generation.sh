562962dde8be63a4fe78a41ebecb79c23fad5867
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-04-13 16:38:36 +0200

    [fix]: commit changes with git authentication

--- scripts/run-org-generation.sh
+++ scripts/run-org-generation.sh
@@ -2,7 +2,7 @@
 
 PATH=~/.roswell/bin:$PATH make org-gen
 
-git config user.email "ci@heliax.dev"
+git config user.email "gianmarco@heliax.dev"
 git config user.name "Drone CI"
 
 REMOTE=$(git remote get-url origin | cut -c 9-)
@@ -12,8 +12,9 @@ if [ -z "$(git status doc/Code --porcelain)" ]; then
   exit 0
 else
   echo "Committing differences..."
+  git remote set-url origin $PUSH_URL
   git add -u
-  git commit -m "[skip ci] run org generation"
-  git push $PUSH_URL
+  git commit -m "run org-generation [CI SKIP]"
+  git push --verbose
   exit $?
 fi
