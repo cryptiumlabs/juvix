d531aa0878b88a5767546204e76dfff68f3ebd27
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-04-13 15:14:31 +0200

    [fix]: push authentication

--- scripts/run-formatter.sh
+++ scripts/run-formatter.sh
@@ -2,13 +2,18 @@
 
 make format
 
+git config user.email "ci@heliax.dev"
+git config user.name "Drone CI"
+
+REMOTE=$(git remote get-url origin | cut -c 9-)
+PUSH_URL="https://${GITHUB_TOKEN}@${REMOTE}.git"
+
 if [ -z "$(git status src/ library/ test/ --untracked-files=no --porcelain)" ]; then
   exit 0
 else
-  # ormolu --version
-  # git status
+  echo "Committing differences..."
   git add -u
-  git commit -m "[CI] Run code formatting"
-  git push
+  git commit -m "[skip ci] run formatter"
+  git push $PUSH_URL
   exit 0
 fi
--- scripts/run-org-generation.sh
+++ scripts/run-org-generation.sh
@@ -2,11 +2,18 @@
 
 PATH=~/.roswell/bin:$PATH make org-gen
 
+git config user.email "ci@heliax.dev"
+git config user.name "Drone CI"
+
+REMOTE=$(git remote get-url origin | cut -c 9-)
+PUSH_URL="https://${GITHUB_TOKEN}@${REMOTE}.git"
+
 if [ -z "$(git status doc/Code --porcelain)" ]; then
   exit 0
 else
+  echo "Committing differences..."
   git add -u
-  git commit -m "[CI] Run org generation"
-  git push
+  git commit -m "[skip ci] Run org generation"
+  git push $PUSH_URL
   exit 0
 fi
