761032a9963afc69a377d427c6f2dbf95e25317d
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-04-13 16:22:16 +0200

    [feat]: logging

--- scripts/run-formatter.sh
+++ scripts/run-formatter.sh
@@ -8,10 +8,13 @@ git config user.name "Drone CI"
 REMOTE=$(git remote get-url origin | cut -c 9-)
 PUSH_URL="https://${GITHUB_TOKEN}@${REMOTE}"
 
+echo $REMOTE
+
 if [ -z "$(git status src/ library/ test/ --untracked-files=no --porcelain)" ]; then
   exit 0
 else
   echo "Committing differences..."
+  git remote -v
   git remote set-url origin $PUSH_URL
   git remote -v
   git add -u
