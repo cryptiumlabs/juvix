c2e3996c8535687a2a1e83c3b07cddd6fbe048e1
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-04-14 11:19:24 +0200

    [feat]: update signature

--- scripts/run-formatter.sh
+++ scripts/run-formatter.sh
@@ -1,22 +1,3 @@
 #!/bin/sh
 
 make format
-
-git config user.email "ci@heliax.dev"
-git config user.name "Drone CI"
-
-REMOTE=$(git remote get-url origin | cut -c 9-)
-PUSH_URL="https://${GITHUB_TOKEN}@${REMOTE}"
-
-if [ -z "$(git status src/ library/ test/ --untracked-files=no --porcelain)" ]; then
-  exit 0
-else
-  echo "Committing differences..."
-  git remote set-url origin $PUSH_URL
-  git fetch --all
-  git fetch $DRONE_SOURCE_BRANCH $DRONE_SOURCE_BRANCH
-  git add -u
-  git commit -m "run formatter [CI SKIP]"
-  git push --verbose
-  exit $?
-fi
--- scripts/run-org-generation.sh
+++ scripts/run-org-generation.sh
@@ -1,22 +1,3 @@
 #!/bin/sh
 
 PATH=~/.roswell/bin:$PATH make org-gen
-
-git config user.email "gianmarco@heliax.dev"
-git config user.name "Drone CI"
-
-REMOTE=$(git remote get-url origin | cut -c 9-)
-PUSH_URL="https://${GITHUB_TOKEN}@${REMOTE}"
-
-if [ -z "$(git status doc/Code --porcelain)" ]; then
-  exit 0
-else
-  echo "Committing differences..."
-  git remote set-url origin $PUSH_URL
-  git fetch $DRONE_SOURCE_BRANCH $DRONE_SOURCE_BRANCH
-  git checkout $DRONE_SOURCE_BRANCH
-  git add -u
-  git commit -m "run org-generation [CI SKIP]"
-  git push
-  exit $?
-fi
