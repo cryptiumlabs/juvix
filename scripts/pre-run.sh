e29c03610a72274063cef7c453194509c7245044
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2021-04-14 17:02:22 +0200

    [feat]: update signature

--- scripts/pre-run.sh
+++ scripts/pre-run.sh
@@ -1,7 +1,5 @@
-echo "c60737030fb788f795abb1b014c49eafa3faa7e1cc9d76088552dfc68d3e11ef  scripts/pre-run.sh" | sha256sum -c -
 echo "2a75d94d23168136b3870a3a480388a7a5fddbf55ae9c57c6dca14c33ab43097  scripts/push-changes.sh" | sha256sum -c -
 echo "a169df616d5cdf363cb7aaaf218a979d66c8c0a8c24e6c7333483dc933138354  scripts/format-and-org-gen.sh" | sha256sum -c -
-echo "a2cb891077e670ccf9546df3ad1e7629e9e8858680a281bc836abfcef414641a  scripts/run-org-generation.sh" | sha256sum -c -
 echo "4438c2dfcd3aa0e4a3700fb5865c9b8e9bd208c38b1cb52b91b5393f56571a03  scripts/check-formatting.sh" | sha256sum -c -
 echo "13f9fae7f558567336505324e4c54dabe978ba7441617854dd31d9f9e9c85c60  scripts/check-org-gen.sh" | sha256sum -c -
 
