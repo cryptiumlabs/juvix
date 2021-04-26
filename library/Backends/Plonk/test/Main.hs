163ad8b86c88809d5335b342136a846ef5d012a2
Alberto Centelles <centelles.alberto@gmail.com>
2021-04-26 12:58:09 +0100

    Remove Groth tests

--- library/Backends/Plonk/test/Main.hs
+++ library/Backends/Plonk/test/Main.hs
@@ -2,10 +2,9 @@ module Main where
 
 import Juvix.Library
 import qualified Test.Compiler as Compiler
-import qualified Test.Groth as Groth
 import qualified Test.Tasty as T
 
 main :: IO ()
 main =
   T.defaultMain $
-    T.testGroup "Plonk tests" [Compiler.top, Groth.top]
+    T.testGroup "Plonk tests" [Compiler.top]
