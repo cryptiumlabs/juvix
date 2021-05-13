b250a5f2eead069e8cfdb3d964d60b46be4687da
drone <noreply@drone>
2021-05-13 08:08:58 +0000

    [ci] changes from CI

--- library/EasyPipeline/src/Easy.hs
+++ library/EasyPipeline/src/Easy.hs
@@ -69,7 +69,7 @@ defMichelson :: Options
 defMichelson =
   def
     { prelude =
-      -- TODO: Avoid relative paths
+        -- TODO: Avoid relative paths
         [ "../../stdlib/Prelude.ju",
           "../../stdlib/Michelson.ju",
           "../../stdlib/MichelsonAlias.ju"
@@ -85,8 +85,6 @@ defCircuit =
           "../../stdlib/Circuit.ju"
         ]
     }
-
-
 
 -- These functions help us stop at various part of the pipeline
 
