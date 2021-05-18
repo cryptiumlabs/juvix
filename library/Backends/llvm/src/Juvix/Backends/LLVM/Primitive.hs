5d21e70048502111ceaa9b33e7486a0bb412a6af
Frank Dedden <frank@heliax.dev>
2021-05-18 12:23:58 +0200

    Added PrimVal wrapper around RawPrimVal.

--- library/Backends/llvm/src/Juvix/Backends/LLVM/Primitive.hs
+++ library/Backends/llvm/src/Juvix/Backends/LLVM/Primitive.hs
@@ -1,16 +1,26 @@
 -- | Representation of LLVM primitives in Juvix.
 module Juvix.Backends.LLVM.Primitive
   ( RawPrimVal (..),
+    PrimVal,
     arityRaw,
   )
 where
 
+import qualified Juvix.Core.Application as App
+import qualified Juvix.Core.Parameterisation as Param
 import Juvix.Library
+
+-- | TODO: Just a placeholder for now.
+data PrimTy
 
 -- | Raw representation of some primitives of LLVM.
 data RawPrimVal
   = Add
   | Sub
+
+-- | The primitive values as exposed to users of Juvix, wrapping inside a
+-- return or a continuation.
+type PrimVal ext = App.Return' ext (Param.PrimType PrimTy) RawPrimVal
 
 -- | Arity of `RawPrimVal`.
 arityRaw :: RawPrimVal -> Natural
