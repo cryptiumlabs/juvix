e93710d3d8149b52b7b1ce1b22432ddb23526393
Frank Dedden <frank@heliax.dev>
2021-05-18 12:25:21 +0200

    Added basic CanApply instance for PrimVal, just `arity` implemented.

--- library/Backends/llvm/src/Juvix/Backends/LLVM/Parameterization.hs
+++ library/Backends/llvm/src/Juvix/Backends/LLVM/Parameterization.hs
@@ -1,4 +1,14 @@
+-- | Parameterization and application of the LLVM backend primitives.
 module Juvix.Backends.LLVM.Parameterization
   (
   )
 where
+
+import Juvix.Backends.LLVM.Primitive
+import qualified Juvix.Core.Application as App
+import qualified Juvix.Core.Parameterisation as Param
+
+instance Param.CanApply (PrimVal ext) where
+  arity val = case val of
+    App.Cont {} -> App.numLeft val
+    App.Return {} -> arityRaw $ App.retTerm val
