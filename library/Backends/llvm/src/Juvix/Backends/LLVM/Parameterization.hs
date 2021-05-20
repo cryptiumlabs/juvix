7c2eeee1b43dc1ed5ca6ed78cd91575cc7a0a009
Frank Dedden <frank@heliax.dev>
2021-05-20 12:35:56 +0200

    Added `add` and `sub` to `builtinTypes`.

--- library/Backends/llvm/src/Juvix/Backends/LLVM/Parameterization.hs
+++ library/Backends/llvm/src/Juvix/Backends/LLVM/Parameterization.hs
@@ -11,6 +11,7 @@ import Juvix.Backends.LLVM.Primitive
 import qualified Juvix.Core.Application as App
 import qualified Juvix.Core.Parameterisation as Param
 import Juvix.Library
+import qualified LLVM.AST.Type as LLVM
 
 instance Param.CanApply (PrimVal ext) where
   arity val = case val of
@@ -22,13 +23,18 @@ llvm :: Param.Parameterisation PrimTy RawPrimVal
 llvm =
   Param.Parameterisation
     { Param.hasType = undefined,
-      Param.builtinTypes = undefined,
+      Param.builtinTypes = builtinTypes,
       Param.builtinValues = builtinValues,
       Param.stringVal = undefined,
       Param.intVal = undefined,
       Param.floatVal = undefined
     }
   where
+    -- The primitive LLVM types available to Juvix users.
+    builtinTypes :: Param.Builtins PrimTy
+    builtinTypes =
+      [("LLVM.int8", LLVM.i8)]
+
     -- The primitive LLVM values available to Juvix users.
     builtinValues :: Param.Builtins RawPrimVal
     builtinValues =
