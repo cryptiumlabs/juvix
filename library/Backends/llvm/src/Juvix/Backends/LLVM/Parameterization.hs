8a1efab28862bad78f591035ebeccd10bfc95500
Frank Dedden <frank@heliax.dev>
2021-05-20 12:10:14 +0200

    Added `add` and `sub` to `builtinValues`.

--- library/Backends/llvm/src/Juvix/Backends/LLVM/Parameterization.hs
+++ library/Backends/llvm/src/Juvix/Backends/LLVM/Parameterization.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | Parameterization and application of the LLVM backend primitives.
 module Juvix.Backends.LLVM.Parameterization
   ( llvm,
@@ -20,8 +23,15 @@ llvm =
   Param.Parameterisation
     { Param.hasType = undefined,
       Param.builtinTypes = undefined,
-      Param.builtinValues = undefined,
+      Param.builtinValues = builtinValues,
       Param.stringVal = undefined,
       Param.intVal = undefined,
       Param.floatVal = undefined
     }
+  where
+    -- The primitive LLVM values available to Juvix users.
+    builtinValues :: Param.Builtins RawPrimVal
+    builtinValues =
+      [ ("LLVM.add", Add),
+        ("LLVM.sub", Sub)
+      ]
