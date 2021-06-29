9d70e4ca1364e46318c41ee63fad7e65a4ff7e49
Alberto Centelles <centelles.alberto@gmail.com>
2021-06-29 11:53:11 +0100

    Comment IRAnn out

--- library/Core/src/Juvix/Core/IRAnn.hs
+++ library/Core/src/Juvix/Core/IRAnn.hs
@@ -1,8 +1,8 @@
 module Juvix.Core.IRAnn
-  ( module Juvix.Core.IRAnn.Types,
-    module Juvix.Core.IRAnn.Erasure,
-  )
+  -- ( module Juvix.Core.IRAnn.Types,
+  --   module Juvix.Core.IRAnn.Erasure,
+  -- )
 where
 
-import Juvix.Core.IRAnn.Erasure
-import Juvix.Core.IRAnn.Types
+-- import Juvix.Core.IRAnn.Erasure
+-- import Juvix.Core.IRAnn.Types
--- library/Core/src/Juvix/Core/IRAnn/Erasure.hs
+++ library/Core/src/Juvix/Core/IRAnn/Erasure.hs
@@ -1,11 +1,11 @@
 module Juvix.Core.IRAnn.Erasure where
 
-import qualified Juvix.Core.IR.TransformExt as Trans
-import qualified Juvix.Core.IR.Types as IR
-import qualified Juvix.Core.IRAnn.Types as IRAnn
+-- import qualified Juvix.Core.IR.TransformExt as Trans
+-- import qualified Juvix.Core.IR.Types as IR
+-- import qualified Juvix.Core.IRAnn.Types as IRAnn
 
-eraseTerm :: forall primTy primVal. IRAnn.Term primTy primVal -> IR.Term primTy primVal
-eraseTerm = Trans.extForgetT
+-- eraseTerm :: forall primTy primVal. IRAnn.Term primTy primVal -> IR.Term primTy primVal
+-- eraseTerm = Trans.extForgetT
 
-eraseElim :: forall primTy primVal. IRAnn.Elim primTy primVal -> IR.Elim primTy primVal
-eraseElim = Trans.extForgetE
+-- eraseElim :: forall primTy primVal. IRAnn.Elim primTy primVal -> IR.Elim primTy primVal
+-- eraseElim = Trans.extForgetE
